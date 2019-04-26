7af3818abdd70aea8f36123f81ae92249c08b705
Iago Bruno <httpiago@gmail.com>
2019-04-26 03:05:45 -0300

    chore: Add dotenv and move sensitive configs to .env

--- database/index.ts
+++ database/index.ts
@@ -1,11 +1,27 @@
 import * as knex from 'knex'
-const configs = require('../knexfile.js')
-const environment = process.env.ENVIRONMENT || 'development'
+require('dotenv').config()
 
 /**
  * Query database.
  * @see https://knexjs.org/
  */
-const db = knex(configs[environment])
+const db = knex({
+  client: process.env.DB_DIALECT,
+  connection: {
+    
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    charset:  'utf8'
+  },
+  debug: false,
+  migrations: {
+    directory: __dirname + '/database/migrations',
+  },
+  seeds: {
+    directory: __dirname + '/database/seeds'
+  },
+})
 
 export default db
